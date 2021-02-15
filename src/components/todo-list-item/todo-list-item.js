0230e377d82ae9d3ebb9d25aa3a9325025ca0c63
Vadim Bykov <bvntaev@gmail.com>
2021-02-15 16:45:44 +0300

    feat: create todo

---
+++ src/components/todo-list-item/todo-list-item.js
@@ -0,0 +1,57 @@
+import React, { Component } from 'react';
+import './todo-list-item.css';
+
+export default class TodoListItem extends Component {
+
+  render() {
+    const { label, onDeleted, onToggleDone, onToggleImportant, done, important } = this.props;
+
+    let classNames = 'todo-list-item';
+    if (done) {
+      classNames += ' done';
+    }
+    if (important) {
+      classNames += ' important';
+    }
+
+    return (
+      <span className={classNames}>
+        <span className='todo-list-item-label' onClick={onToggleDone}>
+          {label}
+        </span>
+        <button
+          type='button'
+          className='btn btn-outline-success btn-sm'
+          onClick={onToggleImportant}
+        >
+          <i className='fa fa-exclamation' />
+        </button>
+        <button type='button' className='btn btn-outline-danger btn-sm' onClick={onDeleted}>
+          <i className='fa fa-trash-o' />
+        </button>
+      </span>
+    );
+  }
+}
+
+// const TodoListItemFunc = ({ label, important = false }) => {
+
+//   const style = {
+//     color: important ? 'steelblue' : 'black',
+//     fontWeight: important ? 'bold' : 'normal',
+//   }
+
+//   return (
+//     <span className='todo-list-item'>
+//       <span className='todo-list-item-label' style={style} >{label}</span>
+//       <button type='button' className='btn btn-outline-success btn-sm' >
+//         <i className='fa fa-exclamation' />
+//       </button>
+//       <button type='button' className='btn btn-outline-danger btn-sm' >
+//         <i className='fa fa-trash-o' />
+//       </button>
+//     </span>
+//   );
+// };
+
+// export default TodoListItem;
