64ba06092d804d9eddad7378dce1952a34167587
Vadim Bykov <70739750+Vadim-Bykov@users.noreply.github.com>
2023-09-04 18:11:47 +0300

    feat: 4 - 0 - useEffect and Component Lifecycle

--- src/pages/Home/HomePage.tsx
+++ src/pages/Home/HomePage.tsx
@@ -1,5 +1,57 @@
-import React from 'react';
+'use client';
+
+import React, { useEffect, useState } from 'react';
 
 export function HomePage() {
-  return <div>Home</div>;
+  const [text, setText] = useState('');
+  const [showText, setShowText] = useState(true);
+
+  return (
+    <div>
+      <input
+        type='text'
+        value={text}
+        placeholder='Post...'
+        onChange={(e) => setText(e.target.value)}
+        className='mr-5'
+      />
+
+      <label>
+        <input
+          type='checkbox'
+          checked={showText}
+          onChange={(e) => setShowText(e.target.checked)}
+        />{' '}
+        Show text
+      </label>
+
+      <div className='my-5'>
+        <button onClick={() => setShowText((prev) => !prev)}>
+          {showText ? 'Hide' : 'Show'} text
+        </button>
+      </div>
+
+      {showText && <Text text={text} />}
+    </div>
+  );
+}
+
+function Text({ text }: { text: string }) {
+  // useEffect(() => {
+  //   console.log('mounted and updates every time');
+  // });
+
+  // useEffect(() => {
+  //   console.log('mounted');
+
+  //   return () => {
+  //     console.log('unmounted');
+  //   };
+  // }, []);
+
+  // useEffect(() => {
+  //   console.log('updated', text);
+  // }, [text]);
+
+  return <div>Text: {text}</div>;
 }
