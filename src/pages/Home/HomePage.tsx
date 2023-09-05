ef6595abb37f90b0ba92b68a64398a10c73c71da
Vadim Bykov <70739750+Vadim-Bykov@users.noreply.github.com>
2023-09-05 17:07:44 +0300

    feat: 5 - useMemo vs useCallback

--- src/pages/Home/HomePage.tsx
+++ src/pages/Home/HomePage.tsx
@@ -1,57 +1,78 @@
 'use client';
 
-import React, { useEffect, useState } from 'react';
+import React, { useCallback, useEffect, useMemo, useState } from 'react';
+
+type FibFn = (n: number) => number;
+
+const fib: FibFn = (n) => {
+  return n <= 1 ? n : fib(n - 1) + fib(n - 2);
+};
+
+// const getExpensiveColors = () => {
+//   for (let index = 0; index < 1_000_000_000; index++) {
+//     //  do something
+//   }
+//   return ['white', 'black'];
+// };
 
 export function HomePage() {
-  const [text, setText] = useState('');
-  const [showText, setShowText] = useState(true);
+  const [number, setNumber] = useState(0);
+  const [randomText, setRandomText] = useState('');
+
+  // const fib: FibFn = (n) => {
+  //   return n <= 1 ? n : fib(n - 1) + fib(n - 2);
+  // };
+
+  // useCallback returns a function
+  // const fib: FibFn = useCallback((n) => {
+  //   return n <= 1 ? n : fib(n - 1) + fib(n - 2);
+  // }, []);
+
+  const fibonacciSequenceResult = fib(number);
+  // useMemo returns a value
+  // const fibonacciSequenceResult = useMemo(() => fib(number), [number, fib]);
+
+  // // React understands that fibonacciSequenceResult is a primitive value and useEffect doesn't rerender
+  // useEffect(() => {
+  //   console.log('New fibonacciSequenceResult');
+  // }, [ fibonacciSequenceResult ]);
+
+  // const colors = getExpensiveColors();
+  // // const colors = useMemo(() => getExpensiveColors(), []);
+
+  // // [] === [] false and useEffect rerenders
+  // useEffect(() => {
+  //   console.log('New colors');
+  // }, [colors]);
 
   return (
     <div>
-      <input
-        type='text'
-        value={text}
-        placeholder='Post...'
-        onChange={(e) => setText(e.target.value)}
-        className='mr-5'
-      />
-
-      <label>
+      <div className='mb-10'>
+        <div>Fibonacci Sequence</div>
         <input
-          type='checkbox'
-          checked={showText}
-          onChange={(e) => setShowText(e.target.checked)}
-        />{' '}
-        Show text
-      </label>
-
-      <div className='my-5'>
-        <button onClick={() => setShowText((prev) => !prev)}>
-          {showText ? 'Hide' : 'Show'} text
-        </button>
+          type='text'
+          value={number}
+          placeholder='Fibonacci...'
+          onChange={(e) => {
+            const value = e.target.value;
+            if (typeof +value === 'number') {
+              return setNumber(+value);
+            }
+            return;
+          }}
+        />
+        <div>Fibonacci Sequence result: {fibonacciSequenceResult}</div>
       </div>
 
-      {showText && <Text text={text} />}
+      <div>
+        <div>Random text</div>
+        <input
+          type='text'
+          value={randomText}
+          placeholder='Random text'
+          onChange={(e) => setRandomText(e.target.value)}
+        />
+      </div>
     </div>
   );
-}
-
-function Text({ text }: { text: string }) {
-  // useEffect(() => {
-  //   console.log('mounted and updates every time');
-  // });
-
-  // useEffect(() => {
-  //   console.log('mounted');
-
-  //   return () => {
-  //     console.log('unmounted');
-  //   };
-  // }, []);
-
-  // useEffect(() => {
-  //   console.log('updated', text);
-  // }, [text]);
-
-  return <div>Text: {text}</div>;
 }
