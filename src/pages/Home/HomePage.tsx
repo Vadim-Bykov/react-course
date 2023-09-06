8771b36f6b7adcb80591787f6e8ca3f51a097a9b
Vadim Bykov <70739750+Vadim-Bykov@users.noreply.github.com>
2023-09-06 18:12:11 +0300

    feat: 8 - useContext vs props

--- src/pages/Home/HomePage.tsx
+++ src/pages/Home/HomePage.tsx
@@ -1,88 +1,78 @@
 'use client';
 
-import React, { Reducer, useReducer, useState } from 'react';
+import React, {
+  ChangeEventHandler,
+  Dispatch,
+  Reducer,
+  useContext,
+  useReducer,
+  useState,
+} from 'react';
 
 type Colors = 'gold' | 'black';
 
-// interface State {
-//   color: Colors;
-//   randomText: string;
-//   count: number;
+interface State {
+  color: Colors;
+  randomText: string;
+  count: number;
+}
+
+enum ActionType {
+  ChangeText = 'CHANGE_TEXT',
+  ChangeColor = 'CHANGE_COLOR',
+  Increment = 'INCREMENT',
+  Decrement = 'DECREMENT',
+}
+
+interface Action {
+  type: ActionType;
+  payload?: any;
+}
+
+const reducer: Reducer<State, Action> = (state, action) => {
+  switch (action.type) {
+    case ActionType.ChangeText:
+      return { ...state, randomText: action.payload };
+
+    case ActionType.Increment:
+      return { ...state, count: state.count + 1 };
+
+    case ActionType.Decrement:
+      return { ...state, count: state.count - 1 };
+
+    case ActionType.ChangeColor:
+      return { ...state, color: state.color === 'gold' ? 'black' : 'gold' };
+
+    default:
+      return state;
+  }
+};
+
+const initialState: State = {
+  color: 'black',
+  randomText: '',
+  count: 0,
+};
+
+// interface Context {
+//   state: State;
+//   dispatch: Dispatch<Action>;
 // }
 
-// enum ActionType {
-//   ChangeText = 'CHANGE_TEXT',
-//   ChangeColor = 'CHANGE_COLOR',
-//   Increment = 'INCREMENT',
-//   Decrement = 'DECREMENT',
-// }
+// const Context = React.createContext<Context>({
+//   state: initialState,
+//   dispatch: () => {},
+// });
 
-// interface Action {
-//   type: ActionType;
-//   payload?: any;
-// }
-
-// const reducer: Reducer<State, Action> = (state, action) => {
-//   switch (action.type) {
-//     case ActionType.ChangeText:
-//       return { ...state, randomText: action.payload };
-
-//     case ActionType.Increment:
-//       return { ...state, count: state.count + 1 };
-
-//     case ActionType.Decrement:
-//       return { ...state, count: state.count - 1 };
-
-//     case ActionType.ChangeColor:
-//       return { ...state, color: state.color === 'gold' ? 'black' : 'gold' };
-
-//     default:
-//       return state;
-//   }
-// };
-
-// const initialState: State = {
-//   color: 'black',
-//   randomText: '',
-//   count: 0,
-// };
+// const useAppContext = () => useContext(Context);
 
 export function HomePage() {
-  const [randomText, setRandomText] = useState('');
-  const [color, setColor] = useState<Colors>('black');
-  const [count, setCount] = useState(0);
-
-  // const [state, dispatch] = useReducer(reducer, initialState);
+  const [state, dispatch] = useReducer(reducer, initialState);
 
   return (
-    <>
+    // <Context.Provider value={{ dispatch, state }}>
+    <div>
       <div>
-        <input
-          type='text'
-          placeholder='Use state text'
-          value={randomText}
-          onChange={(e) => setRandomText(e.target.value)}
-        />
-
-        <div className='mt-3'>
-          <p style={{ color }}>Count: {count}</p>
-          <button className='mr-5' onClick={() => setCount((prev) => prev - 1)}>
-            -
-          </button>
-          <button className='mr-5' onClick={() => setCount((prev) => prev + 1)}>
-            +
-          </button>
-          <button
-            onClick={() => {
-              setColor(color === 'gold' ? 'black' : 'gold');
-            }}
-          >
-            Color
-          </button>
-        </div>
-      </div>
-
-      {/* <div className='mt-10'>
         <input
           type='text'
           placeholder='Use reducer text'
@@ -94,6 +84,7 @@ export function HomePage() {
             })
           }
         />
+
         <div className='mt-3'>
           <p style={{ color: state.color }}>Count: {state.count}</p>
           <button
@@ -112,7 +103,81 @@ export function HomePage() {
             Color
           </button>
         </div>
-      </div> */}
-    </>
+
+        {/* <InputComponent
+          text={state.randomText}
+          onChange={(e) =>
+            dispatch({
+              type: ActionType.ChangeText,
+              payload: e.target.value,
+            })
+          }
+        />
+        <p>{state.randomText}</p> */}
+
+        {/* <ButtonBlock /> */}
+      </div>
+    </div>
   );
 }
+
+// const InputComponent = ({
+//   text,
+//   onChange,
+// }: {
+//   text: string;
+//   onChange: ChangeEventHandler<HTMLInputElement>;
+// }) => {
+//   return (
+//     <>
+//       <div>Description and some content</div>
+
+//       <Field text={text} onChange={onChange} />
+//     </>
+//   );
+// };
+
+// const Field = ({
+//   text,
+//   onChange,
+// }: {
+//   text: string;
+//   onChange: ChangeEventHandler<HTMLInputElement>;
+// }) => {
+//   const {
+//     state: { randomText },
+//   } = useAppContext();
+//   return (
+//     <input
+//       type='text'
+//       placeholder='Use reducer text'
+//       value={randomText}
+//       onChange={onChange}
+//     />
+//   );
+// };
+
+// const ButtonBlock = () => {
+//   const { state, dispatch } = useAppContext();
+
+//   return (
+//     <div className='mt-3'>
+//       <p style={{ color: state.color }}>Count: {state.count}</p>
+//       <button
+//         className='mr-5'
+//         onClick={() => dispatch({ type: ActionType.Decrement })}
+//       >
+//         -
+//       </button>
+//       <button
+//         className='mr-5'
+//         onClick={() => dispatch({ type: ActionType.Increment })}
+//       >
+//         +
+//       </button>
+//       <button onClick={() => dispatch({ type: ActionType.ChangeColor })}>
+//         Color
+//       </button>
+//     </div>
+//   );
+// };
